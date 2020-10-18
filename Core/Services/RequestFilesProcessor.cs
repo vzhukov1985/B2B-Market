afe2210b1ce0e102f0e85fbc649145d02d62ba51
Viktor Zhukov <v_zhukov1985@mail.ru>
2020-10-18 22:53:56 +0600

    Sync

---
+++ Core/Services/RequestFilesProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using Core.Models;
+
+namespace Core.Services
+{
+    public static class RequestFilesProcessor
+    {
+        public static bool CreateNewRequestFileForSupplierLocally(RequestForConfirmation request)
+        {
+            try
+            {
+                using (FileStream fs = File.OpenWrite(
+                    $"{CoreSettings.b2bDataLocalDir}{CoreSettings.SuppliersPath}/{request.FTPSupplierFolder}{CoreSettings.SupplierOrdersPath}/order{request.Code}.xml"))
+                {
+                    XMLProcessor.SaveRequestXMLToStream(request, fs);
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
